b3ad14fa67dd0ed3ae2400cc0c533a7407a29d06
Nick Mew <nick.mew@gmail.com>
2015-04-02 09:46:11 -0700

    Switched standing scrapper to scrape from nba.com.

--- nba_standings_scraper.py
+++ nba_standings_scraper.py
@@ -9,9 +9,8 @@ logger = logging.getLogger('NSS')
 def scrape_standings(group="division"):
     logger.info("begin standings scrape")
     urls = dict(
-        league="http://espn.go.com/nba/standings/_/group/1",
-        conference="http://espn.go.com/nba/standings/_/group/2",
-        division="http://espn.go.com/nba/standings/_/sort/gamesBehind/group/3")
+        conference="http://www.nba.com/standings/team_record_comparison/conferenceNew_Std_Cnf.html",
+        division="http://www.nba.com/standings/team_record_comparison/conferenceNew_Std_Div.html")
     try:
         standings_html = urllib2.urlopen(urls[group], timeout=2).read()
     except urllib2.URLError, e:
@@ -27,29 +26,19 @@ def scrape_standings(group="division"):
         logger.warning("timeout, trying again")
         standings_html = urllib2.urlopen(urls[group], timeout=3).read()
 
-    soup = BeautifulSoup(standings_html)
-    teams = soup.select('table.tablehead > tr[align] > td[align] > a')
+    soup = BeautifulSoup(standings_html.replace('<html class="ie9">', '').replace('<html class="ie8">', ''))
+    teams = soup.select('td.team > a')
     standings = {}
     rank = 0
     for team in teams:
-        games_behind = team.findParent().findNextSibling().findNextSibling().findNextSibling().findNextSibling().string
-        half_game = u'\u00bd' in games_behind
-        games_behind = ''.join([i if ord(i) < 128 else '' for i in games_behind.replace(' ', '')])
-        if half_game:
-            games_behind += '.5'
-        elif '-' not in games_behind:
-            games_behind += '.0'
-
-        standings[team.string] = dict(
+        standings[team.string.replace('.', '')] = dict(
+            clinched="x" in str(team.findParent()) or "e" in str(team.findParent()) or "w" in str(team.findParent()),
+            elimintated="o" in str(team.findParent()),
             wins=team.findParent().findNextSibling().string,
-            clinched="x - " in str(team.findParent()),
             losses=team.findParent().findNextSibling().findNextSibling().string,
             percent=team.findParent().findNextSibling().findNextSibling().findNextSibling().string,
-            behind=games_behind,
-            streak=team.findParent().findNextSibling().findNextSibling().findNextSibling()
-                        .findNextSibling().findNextSibling().findNextSibling()
-                        .findNextSibling().findNextSibling().findNextSibling()
-                        .findNextSibling().findNextSibling().findNextSibling().string,
+            behind=team.findParent().findNextSibling().findNextSibling().findNextSibling().findNextSibling().string,
+            streak=team.findParent().findNextSibling().findNextSibling().findNextSibling().findNextSibling().findNextSibling().findNextSibling().findNextSibling().findNextSibling().findNextSibling().findNextSibling().string,
             division_rank=rank
         )
         rank += 1
