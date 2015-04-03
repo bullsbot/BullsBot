ed28df44f8d71d0ded820527a7ef5694f4e12d60
Nick Mew <nick.mew@gmail.com>
2015-04-02 17:48:32 -0700

    Some comments for the settings file

--- settings.py
+++ settings.py
@@ -38,20 +38,34 @@ schedule = dict(
     max_events_to_display=14,
     prior_events_to_display=3,
     min_events_to_display=10,
-    sidebar_schedule_start_string="* **Schedule**",
-    sidebar_schedule_end_string="\n\n",
+    sidebar_schedule_start_string="> \n",   # the schedule begins after this line(s)
+    sidebar_schedule_end_string="\n\n",     # and ends just before this line(s)
+    # event_fmt is for events in the calendar like [NBA Draft]{#DRAFT} [MONTH DAY]{#DESC}
     event_fmt="[{event_title}]({event_hashtag}) [{month_day}](#DESC)\n",
-    past_game_fmt="[{game_status}](#STATUS) [{away_team_short}](#TEAM) [{away_score}](#SCORE) [{home_team_short}](#TEAM2) [{home_score}](#SCORE2) [{month_day}](#DATE)\n",
-    current_game_fmt="[{game_status}](#STATUS) [{away_team_short}](#TEAM) [{away_score}](#SCORE) [{home_team_short}](#TEAM2) [{home_score}](#SCORE2) [{month_day}](#DATE)\n",
-    future_game_fmt="[{month_day}](#STATUS) [{away_team_short}](#TEAM) [](#SCORE) [{home_team_short}](#TEAM2) [](#SCORE2) [{game_time_local}](#DATE)\n",
+    # GAME FMT OPTIONS:
+    # {away_score} {away_team_short}
+    # {home_score} {home_team_short} {month_day} {game_status} {game_time_local}
+    # FYI: the winning team will get ** around their 'team_short'
+    past_game_fmt="[{away_team_short}](#T1)[{away_score}](#S1)[{home_team_short}](#T2)[{home_score}](#S2)[FINAL](#ST)[{month_day}](#DT)\n",
+    current_game_fmt="[{away_team_short}](#T1)[{away_score}](#S1)[{home_team_short}](#T2)[{home_score}](#S2)[{game_status}](#ST)[{month_day}](#DT)\n",
+    future_game_fmt="[{away_team_short}](#T1)[](#S1)[{home_team_short}](#T2)[](#S2)[{month_day}](#ST)[{game_time_local}](#DT)\n",
+    # game_thread_link is added before past_game_fmt and current_game_fmt
     game_thread_link_fmt="[]({link})",
-    event_markdown_pre=" - ", # goes before each game/event
-    event_markdown_post="", # goes after each game/event
+    event_markdown_pre=" - ",   # goes before each game/event
+    event_markdown_post="",     # goes after each game/event
 )
 
 thread = dict(
+    # POST GAME THREAD OPTIONS:
+    # {home_team_short} {home_team_name} {home_team_win_loss} {home_score} {full_date} {link_date}
+    # {home_team_short} {away_team_name} {away_team_win_loss} {away_score}
     post_game_thread_fmt="HOME TEAM|FINAL SCORE|AWAY TEAM\n:--:|:--:|:--:\n[](#{home_team_short}){home_team_name}*{home_team_win_loss}*|**{home_score}-{away_score}** *{full_date}* *[BOX SCORE](http://www.nba.com/games/{link_date}/{away_team_short}{home_team_short}/gameinfo.html#nbaGIboxscore)*|[](#{away_team_short}){away_team_name}*{away_team_win_loss}*\n",
+
+    # POST GAME THREAD TITLE OPTIONS:
+    # {sub_team_name} {sub_team_win_loss} {sub_score} {beat_or_lose}
+    # {non_sub_team_name} {non_sub_team_win_loss} {non_sub_score}
     post_game_thread_title_fmt="POST GAME: {sub_team_name} ({sub_team_win_loss}) {beat_or_lose} {non_sub_team_name} ({non_sub_team_win_loss}) ({sub_score}-{non_sub_score})",
+
     pre_game_thread_fmt="HOME TEAM|INFORMATION|AWAY TEAM\n:--:|:--:|:--:\n[](#{home_team_short}){home_team_name}*{home_team_win_loss}*|*{full_date}*|[](#{away_team_short}){away_team_name}*{away_team_win_loss}*\n\n[](#empty)|DETAILED OVERVIEW|[](#empty)\n:--|:--|:--\n[](#empty)|*BROADCAST* {broadcast}|[](#empty)\n[](#empty)|*GAME TIMES* [Eastern: {game_time_eastern}](#TIME) / [Central: {game_time_central}](#TIME) / [Mountain: {game_time_mountain}](#TIME) / [Pacific:  {game_time_pacific}](#TIME)|[](#empty)\n[](#empty)|*MISC/NOTES* [Game Story](http://www.nba.com/games/{link_date}/{away_team_short}{home_team_short}/gameinfo.html)|[](#empty)\n[](#empty)|*SUBREDDITS* /r/{home_subreddit} / /r/{away_subreddit}|[](#empty)\n",
     pre_game_thread_title_fmt="PRE GAME: {home_team_name} ({home_team_win_loss}) vs. {away_team_name} ({away_team_win_loss}) ({month_day_year})",
     pre_game_date_fmt="%A***%b %d***%Y",
