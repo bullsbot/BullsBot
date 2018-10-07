5385dc3319ee5045f82953857481aaf05211fe02
brexbre <bullsxbre@gmail.com>
2018-10-07 00:44:29 -0500

    disabled game threads

--- bulls_bot.py
+++ bulls_bot.py
@@ -1044,9 +1044,8 @@ def schedule_schedule_updates():
     while run_updates:
         try:
             # try:
-            bot.generate_or_update_game_thread_if_necessary()     # update or create game threads if necessary
+            # bot.generate_or_update_game_thread_if_necessary()     # update or create game threads if necessary
             schedule = bot.generate_default_schedule()            # get schedule
-            # print(schedule)
             bot.update_schedule(schedule)                         # update schedule in sidebar
             update_freq = bot.get_current_update_freq()           # figure out when to update again
             consecutive_error_count = 0
